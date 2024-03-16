0e36bdc05e4571914eb62de60f9f7b0d89bd7113
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-03-16 14:49:30 +0530

    Fix same audio error (use base64)

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, jsonify
 # Custom modules
 import google_handlers
 import gemini
+import base64
+import io
 
 
 app = Flask(__name__)
@@ -24,12 +26,16 @@ def celeb():
 
     response_text, audio_path = gemini.handler(
         celebrity, say_what, model_parameters={}, target_language=target_language, audio_path="./static/audio/response.mp3")
-
     print(f"Response: {response_text}, {audio_path}")
+
+    # Convert the audio to base64
+    with open(audio_path, "rb") as audio_file:
+        audio_bytes = audio_file.read()
+        audio_base64 = base64.b64encode(audio_bytes).decode('utf-8')
 
     result = {
         "response": response_text,
-        "audio_path": audio_path
+        "audio_base64": audio_base64
     }
 
     return render_template("index.html", LANG_MAP=google_handlers.LANG_MAP, result=result)

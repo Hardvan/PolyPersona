60bb1acd9adacd8d7c98bb16bad2cabb16214178
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-03-24 11:18:47 +0530

    FileHarvestor

--- automater.py
+++ automater.py
@@ -1,4 +1,6 @@
 import FlaskStarter
+import FileHarvestor
 
 if __name__ == "__main__":
-    FlaskStarter.create_flask_project()
+    # FlaskStarter.create_flask_project()
+    FileHarvestor.read_files(["app.py", "gemini.py", "google_handlers.py"])

cc661c4a650451299fdb83964e0726988a32cd91
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-03-25 19:03:49 +0530

    Star effect Gemini API

--- static/js/index.js
+++ static/js/index.js
@@ -22,3 +22,53 @@ document.querySelector("h1").onmouseover = (event) => {
     iteration += 1 / 3;
   }, 30); // 30ms
 };
+
+// Magical text effect
+let index2 = 0,
+  interval2 = 5000;
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const animate = (star) => {
+  star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+  star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+  star.style.animation = "none";
+  star.offsetHeight;
+  star.style.animation = "";
+};
+
+for (const star of document.getElementsByClassName("magic-star")) {
+  setTimeout(() => {
+    animate(star);
+
+    setInterval(() => animate(star), 1000);
+  }, index2++ * (interval2 / 3));
+}
+
+/* -- ↓↓↓ If you want the sparkle effect to only occur on hover, replace lines 15 and on with this code ↓↓↓ -- */
+
+// let timeouts = [],
+//     intervals = [];
+
+// const magic = document.querySelector(".magic");
+
+// magic.onmouseenter = () => {
+//   let index = 1;
+
+//   for(const star of document.getElementsByClassName("magic-star")) {
+//     timeouts.push(setTimeout(() => {
+//       animate(star);
+
+//       intervals.push(setInterval(() => animate(star), 1000));
+//     }, index++ * 300));
+//   };
+// }
+
+// magic.onmouseleave = onMouseLeave = () => {
+//   for(const t of timeouts) clearTimeout(t);
+//   for(const i of intervals) clearInterval(i);
+
+//   timeouts = [];
+//   intervals = [];
+// }

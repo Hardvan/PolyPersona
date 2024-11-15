a7f382c5b1d2b633ccaf16afafe286acd8e3c142
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-11-15 17:11:15 +0530

    Remove output folder

--- automater.py
+++ automater.py
@@ -3,4 +3,7 @@ import FileHarvestor
 
 if __name__ == "__main__":
     # FlaskStarter.create_flask_project()
-    FileHarvestor.read_files(["app.py", "gemini.py", "google_handlers.py"])
+    FileHarvestor.read_files(["README.md",
+                              "app.py",
+                              "gemini.py",
+                              "google_handlers.py"])
